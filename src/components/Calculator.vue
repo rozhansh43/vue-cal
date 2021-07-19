33b63813107a2ad3ac3e6d93b39eddaee193af92
rozhan <rozhansheikholmoluki@gmail.com>
2021-07-19 14:33:02 +0430

    checked decimal should be fixed

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,30 +1,19 @@
 <template>
 <div id="app">
   <div class="abs-center calculator">
-    <div class="block display-section">
-      <div class="block display">
-        <input  type="text" v-model="fullFormula" @keyup.enter="execute" >
-        
-        <div class="block small">
-          {{fullFormula}}
-        </div>
-
-        <div class="block">
-          {{(currentInput?currentSign:'')}}{{currentInput||result}}
-        </div>
-      </div>
-    </div>
+    <input  type="text" v-model="fullFormula" @keyup.enter="execute"/>
+    
     <div class="block button-section">
       <div class="flex-r button-row">
         <div class="button grey" @click="clear">
           AC
         </div>
 
-        <div class="button grey" @click="inputClear">
+        <div class="button grey" @click="clear">
           C
         </div>
 
-        <div class="button" @click="changeSign()">
+        <div class="button" @click="changeSign">
           +/-
         </div>
         <div class="button" @click="addOperator('/')">/</div>
@@ -102,76 +91,78 @@
 
 <script>
 export default {
-  data() {
+  name: 'Calculator',
+  data () {
     return {
       currentSign: '',
       currentInput: '',
       fullFormula: '',
       result: '',
       inputResult: ''
-    };
+    }
   },
   methods: {
     updateNumber (digit) {        
-      if(digit === '.') {
-        if(!this.currentInput || !(this.currentInput*1)) { 
+      if (digit === '.') {
+        if (!this.currentInput || !(this.currentInput * 1)) { 
           this.currentInput = '0'
         }
-        if(!(this.currentInput.includes('.'))) { 
+
+        if (!(this.currentInput.includes('.'))) {
           this.currentInput += '.'
-          }
+        }
       } else {
         (!this.currentInput || (!(this.currentInput*1) && !(this.currentInput.includes('.')))) ? (
-            this.currentInput = digit
+            this.currentInput += digit,
+            this.fullFormula += digit
         ):(
-            this.currentInput += digit
+            this.currentInput += digit,
+            this.fullFormula += digit
         );
       }
     },
     changeSign () {
-      (this.currentSign)?(this.currentSign = ''):(this.currentSign = '-')
+      this.currentSign ? this.currentSign = '' : this.currentSign = '-'
     },
     addOperator (op) {
       if(this.result) {
-        this.fullFormula = (this.result +' '+ op +' ')
+        this.fullFormula = `${this.result} ${op} `
         this.result = ''
       } else {
         if (this.currentInput) {
-          this.fullFormula += (this.currentSign + this.currentInput +' '+ op +' ')
+          this.fullFormula += `${this.currentSign} ${op} `
           this.currentSign = ''
           this.currentInput = ''
         } else {
-          if(this.fullFormula.includes(' ')) {
+          if (this.fullFormula.includes(' ')) {
             var temp = this.fullFormula.split('')
             temp[temp.length-2] = op
             this.fullFormula = temp.join('')
           }
         }
       }
+      
     },
     execute () {
       if(this.fullFormula) {
-        this.fullFormula += (this.currentSign + this.currentInput);
-        this.result = eval(this.fullFormula);
+        this.fullFormula
+        this.result = eval(this.fullFormula)
       } else {
-        this.fullFormula = this.currentSign + this.currentInput;
-        this.result = this.currentInput;
+        this.fullFormula
+        this.result = this.currentInput
       }
+      
       this.currentSign = ''
       this.currentInput = ''
+      
       this.fullFormula = this.result 
-
     },
     clear () {
       this.currentSign = ''
       this.currentInput = ''
       this.fullFormula = ''
       this.result = ''
-    },
-    inputClear () {
-      this.currentSign = '';
-      this.currentInput = '';
-    },
-  },
+    }
+  }
 };
 </script>
